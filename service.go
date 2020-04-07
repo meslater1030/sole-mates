2d47cba75335957a1bace7b7fc8583fcba1b91df
Megan E Slater <megan.e.slater@gmail.com>
2020-04-06 17:25:17 -0700

    Add ability to connect to db

--- service.go
+++ service.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -13,5 +17,10 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+	datasourceName := os.Getenv("SQL_DATASOURCE_NAME")
+	_, err := sql.Open("cloudsqlpostgres", datasourceName)
+	log.Print(datasourceName)
+	log.Print(err)
 	http.ListenAndServe(":3000", r)
+	log.Print(postgres.Driver{})
 }
